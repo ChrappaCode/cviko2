8ac9cf43d1e5e219dc796ac403359920bbee9f81
Jakub <xchrappaj@stuba.sk>
2022-02-23 16:00:20 +0100

    CvikoPorovnavanie

--- src/main/java/sk/stuba/fei/uim/oop/utility/CvikoPorovnavanie.java
+++ src/main/java/sk/stuba/fei/uim/oop/utility/CvikoPorovnavanie.java
@@ -1,11 +1,27 @@
 package sk.stuba.fei.uim.oop.utility;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class CvikoPorovnavanie {
 
     public static void main(String[] args) {
 
-        int cislo = 5;
+        Scanner klavesnica = new Scanner(System.in);
+
+        String a = "a";
+        String b = "a";
+        System.out.println("zadaj pismeno:");
+        String c = klavesnica.nextLine();
+
+        System.out.println("a == a:" + (a == a));
+        System.out.println("a == b:" + (a.equals(b)));
+        System.out.println("a == c:" + (a == c));
+
+        System.out.println("Objects.equals(a,a)" + Objects.equals(a,a));
+        System.out.println("Objects.equals(a,b)" + Objects.equals(a,b));
+        System.out.println("Objects.equals(a,c)" + Objects.equals(a,c));
+
 
     }
-
 }
