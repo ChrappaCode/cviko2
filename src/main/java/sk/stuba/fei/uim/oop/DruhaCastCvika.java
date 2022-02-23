029c0a962d3eb0a2aa3f01a8158afffb5b3dfa5e
Jakub <xchrappaj@stuba.sk>
2022-02-23 16:09:45 +0100

    Druha cast cvika funkcie

---
+++ src/main/java/sk/stuba/fei/uim/oop/DruhaCastCvika.java
@@ -0,0 +1,35 @@
+package sk.stuba.fei.uim.oop;
+
+import java.util.Arrays;
+
+public class DruhaCastCvika {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        int[] y = new int[]{1,2,3};
+
+        System.out.println(x);
+        testPrimitive(x);
+        System.out.println(x);
+
+        System.out.println(Arrays.toString(y));
+        testArray(y);
+        System.out.println(Arrays.toString(y));
+
+    }
+
+    public static void testPrimitive (int cislo){
+
+        cislo = 10;
+        System.out.println(cislo);
+
+    }
+
+    public static void testArray(int[] array){
+
+        array[0] = 10;
+        System.out.println(Arrays.toString(array));
+
+    }
+}
