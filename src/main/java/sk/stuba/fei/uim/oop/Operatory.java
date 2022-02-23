7840ef3a1936f1c7d69a4e5617dd212b6152b1b5
Jakub <xchrappaj@stuba.sk>
2022-02-23 13:25:06 +0100

    cvji

---
+++ src/main/java/sk/stuba/fei/uim/oop/Operatory.java
@@ -0,0 +1,28 @@
+package sk.stuba.fei.uim.oop;
+
+public class Operatory {
+    public static void main(String[] args) {
+
+        boolean b = false;
+        int i = 345;
+        int j = 10;
+        String s = "abc";
+
+        System.out.print("int i = " + i + " a int j = " + j + " = ");
+        System.out.println(i + j);
+
+        System.out.println("String s = " + s + " + boolean b = " + b + " = " + (s + b));
+
+        System.out.println("int i = " + i + " + boolean b = " + b + " = " + i + b);
+
+        j = i * (i + 1);
+
+        System.out.println(j > i);
+
+
+
+
+
+
+    }
+}
