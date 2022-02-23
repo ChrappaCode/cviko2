af9211a2c026e382803ddf355daed6e84656de29
Jakub <xchrappaj@stuba.sk>
2022-02-23 13:29:50 +0100

    cvji

---
+++ src/main/java/sk/stuba/fei/uim/oop/Pretypovanie.java
@@ -0,0 +1,18 @@
+package sk.stuba.fei.uim.oop;
+
+public class Pretypovanie {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+        double x = 1.7;
+
+        n = (int)x ; //oreze za ciarkou
+
+        System.out.println("int n = " + n);
+        System.out.println("double x = " + x);
+
+    }
+}
+
+
