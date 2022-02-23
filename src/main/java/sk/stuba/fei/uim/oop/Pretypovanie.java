594fadba015d5d8ee6069a886425778464b94d13
Jakub <xchrappaj@stuba.sk>
2022-02-23 13:58:47 +0100

    cvji

--- src/main/java/sk/stuba/fei/uim/oop/Pretypovanie.java
+++ src/main/java/sk/stuba/fei/uim/oop/Pretypovanie.java
@@ -4,13 +4,20 @@ public class Pretypovanie {
 
     public static void main(String[] args) {
 
+        int vstup = Integer.parseInt(args[0]);
         int n = 5;
         double x = 1.7;
 
         n = (int)x ; //oreze za ciarkou
 
+        System.out.println("int vstup = " + vstup);
         System.out.println("int n = " + n);
+        x++;
         System.out.println("double x = " + x);
+
+        int n2 = 0;
+        boolean b2 = (n != 0) && (5 / n >= 1) ;
+        System.out.println(b2);
 
     }
 }
